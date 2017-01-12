1557e44a66bad2ae6b0704b3d0fb5a700e911e07
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 20:15:33 +0000

    Implemented wrapLog function with callback.

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -1,5 +1,9 @@
 var wrapLog = function (callback, name) {
   /* your code here */
+  return function() {
+    var args = Array.from(arguments);
+    console.log(name + "(" + args + ")" + " => " + callback.apply(callback, args));
+  }
 };
 
 var sum = function (a,b) { return a + b; };
@@ -7,3 +11,4 @@ var logSum = wrapLog(sum, "sum");
 
 logSum(5,3); // sum(5,3) => 8
 logSum(3,2); // sum(3,2) => 5
+logSum(4,9);
