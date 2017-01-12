38f86eef6817ef3ebc6c8b40037a0be935391ecd
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-12 22:03:58 +0000

    Update code to conform with linter.

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -3,12 +3,12 @@ var wrapLog = function (callback, name) {
   return function() {
     var args = Array.from(arguments);
     console.log(name + "(" + args + ")" + " => " + callback.apply(callback, args));
-  }
+  };
 };
 
-var sum = function (a,b) { return a + b; };
+var sum = function (a, b) { return a + b; };
 var logSum = wrapLog(sum, "sum");
 
-logSum(5,3); // sum(5,3) => 8
-logSum(3,2); // sum(3,2) => 5
-logSum(4,9);
+logSum(5, 3); // sum(5,3) => 8
+logSum(3, 2); // sum(3,2) => 5
+logSum(4, 9);
